149484e03fcaac6db72101f434b0bddbb8b7469d
topboyceejay <nsudechinonso200@gmail.com>
2022-07-20 17:09:15 -0700

    adding to my dashboard

---
+++ orders.js
@@ -0,0 +1,33 @@
+const Orders = [
+   
+   {
+    productName: 'Foldable Mini Drone',
+    productNumber: '856321',
+    paymentStatus: 'Due',
+    shipping: 'pending'
+   },
+   {
+    productName: 'Biscuit Partfait',
+    productNumber: '36488',
+    paymentStaus: 'Refunded',
+    shipping: 'declined'
+   },
+   {
+    productName: 'Cake and Ice cream',
+    productNumber: '55363',
+    paymentStaus: 'Due',
+    shipping: 'pending'
+   },
+   {
+    productName: 'Shawama',
+    productNumber: '78621',
+    paymentStaus: 'paid',
+    shipping: 'Delivered'
+   },
+   {
+    productName: 'Meatpies',
+    productNumber: '2345',
+    paymentStaus: 'Paid',
+    shipping: 'Delivered'
+   }
+]
